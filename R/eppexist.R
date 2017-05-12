e77ab88013f8c887c86ab7c970b62bce7a961fd0
Richard Detomasi <RichDeto@users.noreply.github.com>
2017-05-12 19:52:25 -0300

    Update eppexist.R

--- R/eppexist.R
+++ R/eppexist.R
@@ -24,5 +24,5 @@ eppexist <- function(pop,centers, n = 3, m = 0, d1 = 1000, d2 = d1 * 2, crs) {
     }
     remove(cov)
   }
-  eppexist.output <<- list(pop, centers, used_capacity, assigned )
+  eppexist.output <- list("pop" = pop, "centers" = centers, "used_capacity" = used_capacity[[1]], "assigned" = assigned[[1]] )
 } 
