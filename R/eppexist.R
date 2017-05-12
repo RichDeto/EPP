08e985feebea5cdb343af41638186c173038c5b3
Richard Detomasi <RichDeto@users.noreply.github.com>
2017-05-12 13:30:51 -0300

    Update eppexist.R

--- R/eppexist.R
+++ R/eppexist.R
@@ -4,9 +4,9 @@ eppexist <- function(pop,centers, n = 3, m = 0, d1 = 1000, d2 = d1 * 2, crs) {
   dist <- c(rep.int(d1,n), rep.int(d2,m)) ## compiles a vector of distances
   for(i in 1:length(dist)){
     if (nrow(pop) > 0 & nrow(centers) > 0)
-    iteration <- assignation_exist(pop, centers, dist[i], crs) ## assigns the population to the centers
-    pop <- iteration[[1]]
-    cov <- iteration[[2]]
+      iteration <- assignation_exist(pop, centers, dist[i], crs) ## assigns the population to the centers
+      pop <- iteration[[1]]
+      cov <- iteration[[2]]
     if (nrow(cov) > 0){
       cov$it <- i 
       cov$dist <- dist[i]
