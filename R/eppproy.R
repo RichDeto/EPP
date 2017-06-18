7c4587f6fbf7321eaa2a4c22b355af3eb31b8c68
Richard Detomasi <RichDeto@users.noreply.github.com>
2017-06-18 02:03:58 -0300

    Update eppproy.R

--- R/eppproy.R
+++ R/eppproy.R
@@ -1,8 +1,4 @@
 eppproy <- function(pob, m = 5, l = 4, o = 3, g1 = 50, g2 = g1 * 0.5, d1 = 1000, d2 = d1 * 2, crs = CRS("+init=epsg:32721")) {
-  require(sp)
-  require(maptools)
-  require(rgeos)
-  require(rgdal)
   assigned <- assing_clust(clust_it(pop)[[1]])
   eppproy.output <- list (centros_clusters_s <- SpatialPointsDataFrame(SpatialPoints(assigned[[1]][,2:3],crs), 
                                                                        assigned[[1]], match.ID = TRUE), 
