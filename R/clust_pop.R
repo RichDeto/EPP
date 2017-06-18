e92eb33ecce0c78181fe1247a21f7e976036d3d6
Richard Detomasi <RichDeto@users.noreply.github.com>
2017-06-18 01:58:15 -0300

    Update clust_pop.R

--- R/clust_pop.R
+++ R/clust_pop.R
@@ -1,12 +1,9 @@
 clust_pop <- function(pop, k){ 
-  require(flexclust) 
   pop$cluster <- as.factor(cclust(pop[,1:2],k)@cluster)
   medianx <- data.frame(medianx=as.vector(tapply(pop$x,pop$cluster,FUN = median)))
   mediany <- data.frame(mediany=as.vector(tapply(pop$y,pop$cluster,FUN = median)))
   pop <- merge(pop,medianx,by.x = "cluster",by.y = "row.names") 
   pop <- merge(pop,mediany,by.x = "cluster",by.y = "row.names")
-  # Euclidean distance from each observation to the median center of its group
-  pop$dist <- sqrt((pop$x - pop$medianx) ^ 2 + (pop$y - pop$mediany) ^ 2) 
-  #ordered by distance
-  pop <- pop[order(pop$cluster,pop$dist),] 
+  pop$dist <- sqrt((pop$x - pop$medianx) ^ 2 + (pop$y - pop$mediany) ^ 2) # Euclidean distance to the median center of group 
+  pop <- pop[order(pop$cluster,pop$dist),]  #ordered by distance
 }
