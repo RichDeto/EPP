2491aaaf962357e7e065a4fe1a2843d6e72c65e4
Richard Detomasi <RichDeto@users.noreply.github.com>
2021-07-26 09:35:52 -0300

    Update group_over.R

--- R/group_over.R
+++ R/group_over.R
@@ -1,5 +1,6 @@
 #' group_over
 #'
+#' @description grouping of id and capacity of centers who overlapping geometries
 #' @param x A data.frame with unless four variables: x, y, id, capacity
 #'
 #' @return A data.frame without geographic duplcates, aggregating ids and capacity
@@ -17,5 +18,4 @@ group_over <- function(x) {
                 ungroup() %>% 
                 filter(!duplicated(crds)) %>% 
                 select(-crds)
-}
-        
+}        
